98f71c10a4e75fc3c4be95a6749a6b9717515622
sidya diaby gassama <sidyadiaby2016@gmail.com>
2024-04-30 13:05:41 +0200

    modification

--- connec.php
+++ connec.php
@@ -14,3 +14,8 @@ define('USER', 'root');
 define('PASS', '');
 
 $pdo = new \PDO(DSN, USER, PASS);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
